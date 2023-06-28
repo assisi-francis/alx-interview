f12d178b7bc26ef4530c1042b0de1e5529dc2c11
assisi-francis <doncyco123@gmail.com>
2023-06-28 20:17:26 +0100

    change mode

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,19 +5,19 @@ def pascal_triangle(n):
     """
     returns a list of lists of integers
     """
-    triangle = []
-
-    # return trianlgle if n <= 0
     if n <= 0:
-        return triangle
-    for i in range(n):
-        temp_list = []
-
-        for j in range(i+1):
-            if j == 0 or j == i:
-                temp_list.append(1)
-            else:
-                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
-        triangle.append(temp_list)
-    # print
+        return []
+    
+    triangle = [[1]]
+    
+    for i in range(1, n):
+        row = [1]
+        
+        for j in range(1, i):
+            value = triangle[i-1][j-1] + triangle[i-1][j]
+            row.append(value)
+        
+        row.append(1)
+        triangle.append(row)
+    
     return triangle
