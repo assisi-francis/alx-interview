84df118cc2db20f75effa8dc2f36834f57a86d58
assisi-francis <doncyco123@gmail.com>
2023-07-12 22:50:19 +0100

    task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def minOperations(n: int) -> int:
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    factor = 2
+    
+    while factor * factor <= n:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    
+    if n > 1:
+        operations += n
+    
+    return operations
