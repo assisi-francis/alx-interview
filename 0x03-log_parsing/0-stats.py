41e767b704ce5b0d11cb0339b48a8f7ff2abc4ad
assisi-francis <doncyco123@gmail.com>
2023-07-24 14:16:07 +0100

    generator

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,47 @@
 #!/usr/bin/python3
 """ script that reads stdin line by line and computes metrics """
 
+if __name__ == '__main__':
 
-import sys
-import re
-from collections import defaultdict
+    import sys
 
-# Regular expression pattern to match the input format
-pattern = r'^(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)$'
-regex = re.compile(pattern)
+    def print_results(statusCodes, fileSize):
+        """ Print statistics """
+        print("File size: {:d}".format(fileSize))
+        for statusCode, times in sorted(statusCodes.items()):
+            if times:
+                print("{:s}: {:d}".format(statusCode, times))
 
-# Dictionary to store the file sizes and status code counts
-file_sizes = []
-status_counts = defaultdict(int)
+    statusCodes = {"200": 0,
+                   "301": 0,
+                   "400": 0,
+                   "401": 0,
+                   "403": 0,
+                   "404": 0,
+                   "405": 0,
+                   "500": 0
+                   }
+    fileSize = 0
+    n_lines = 0
 
-try:
-    for line_number, line in enumerate(sys.stdin, start=1):
-        # Check if the line matches the input format
-        match = regex.match(line)
-        if not match:
-            continue
-
-        # Extract the file size and status code from the matched groups
-        file_size = int(match.group(4))
-        status_code = match.group(3)
-
-        # Update the total file size
-        file_sizes.append(file_size)
-        total_size = sum(file_sizes)
-
-        # Update the status code counts
-        if status_code.isdigit():
-            status_counts[status_code] += 1
-
-        # Print statistics after every 10 lines
-        if line_number % 10 == 0:
-            print(f"Total file size: {total_size}")
-            for code in sorted(status_counts.keys()):
-                print(f"{code}: {status_counts[code]}")
-            print()
-
-except KeyboardInterrupt:
-    # Print the final statistics upon receiving a keyboard interruption
-    print(f"Total file size: {total_size}")
-    for code in sorted(status_counts.keys()):
-        print(f"{code}: {status_counts[code]}")
-    sys.exit(0)
+    try:
+        """ Read stdin line by line """
+        for line in sys.stdin:
+            if n_lines != 0 and n_lines % 10 == 0:
+                """ After every 10 lines, print from the beginning """
+                print_results(statusCodes, fileSize)
+            n_lines += 1
+            data = line.split()
+            try:
+                """ Compute metrics """
+                statusCode = data[-2]
+                if statusCode in statusCodes:
+                    statusCodes[statusCode] += 1
+                fileSize += int(data[-1])
+            except:
+                pass
+        print_results(statusCodes, fileSize)
+    except KeyboardInterrupt:
+        """ Keyboard interruption, print from the beginning """
+        print_results(statusCodes, fileSize)
+        raise
